abca6704752b85610ea589eb2af7ce2797b77344
Christoph Erdmann <cerdmann@matero.de>
2025-07-31 12:23:05 +0200

    revert add GenericPageTitleProvider autoconfigure attribute

--- src/Provider/GenericPageTitleProvider.php
+++ src/Provider/GenericPageTitleProvider.php
@@ -2,10 +2,8 @@
 
 namespace Materodev\ExtbaseUtilities\Provider;
 
-use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
 use TYPO3\CMS\Core\PageTitle\AbstractPageTitleProvider;
 
-#[Autoconfigure(public: true)]
 class GenericPageTitleProvider extends AbstractPageTitleProvider
 {
     public function setTitle(string $title = ''): void
